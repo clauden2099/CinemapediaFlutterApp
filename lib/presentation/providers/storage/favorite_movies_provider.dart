0b3fcd8931212bd6ba27dcacd3269105a7917d78
Alan <alanjavier852@gmail.com>
2026-03-31 21:52:21 -0600

    Se soluciono error al agregar pelicula a favoritos

--- lib/presentation/providers/storage/favorite_movies_provider.dart
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -8,7 +8,6 @@ final favoriteMoviesProvider = StateNotifierProvider((ref) {
   return StorageMoviesNotifier(localStorageRepository: localStorageRepository);
 });
 
-
 /*El state es un mapa que tiene las peliculas que están acomo favoritas en la BD */
 class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
   int page = 0;
@@ -25,7 +24,7 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
     print('isFavorite $isFavorite');
 
     //Este cambio es solo en el state visible para el usuario
-    if(isFavorite){
+    if (isFavorite) {
       //Se elimina la pelicula del state
       state.remove(movie.id);
       //Se reasigna el state para que así el provider notifique los cambios
@@ -34,17 +33,20 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
       return;
     }
 
-    state = {...state, movie.id:movie};
+    state = {...state, movie.id: movie};
   }
 
-  Future<List<Movie>> loadNextPage() async{
-    final movies = await localStorageRepository.loadFavoriteMovies(limit: 10, offset: page*10);
+  Future<List<Movie>> loadNextPage() async {
+    final movies = await localStorageRepository.loadFavoriteMovies(
+      limit: 10,
+      offset: page * 10,
+    );
     page++;
 
     final tempMovies = <int, Movie>{};
 
     //Convierte la lista de peliculas al mapa de movies que maneja el state
-    for(final movie in movies){
+    for (final movie in movies) {
       //state = {...state, movie.id:movie};
       tempMovies[movie.id] = movie;
     }
@@ -52,6 +54,5 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
     state = {...state, ...tempMovies};
 
     return movies;
-
   }
 }
